077ab0d1746eb061a6deb09a3f19f6c7fb953577
Alina <alina.hasan.aman@gmail.com>
2025-02-03 16:42:44 +0600

    minor update

--- express/app.js
+++ express/app.js
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 app.use(express.static(__dirname));
