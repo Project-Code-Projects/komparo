2f9700741c093a7eaf9c2813cffcd56c0aa7fb65
Alina <alina.hasan.aman@gmail.com>
2025-02-03 16:52:48 +0600

    minor update

--- express/app.js
+++ express/app.js
@@ -21,5 +21,5 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
-    console.log(`Test page available at http://localhost:${PORT}/test.html`);
+    console.log(`Scraper test page available at http://localhost:${PORT}/scraper-test.html`);
 });
