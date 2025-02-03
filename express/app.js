b5f0d6000389ea133c8e2d1a01657b303f97d95e
Alina <alina.hasan.aman@gmail.com>
2025-02-03 16:19:21 +0600

    display scraper products

---
+++ express/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import { fileURLToPath } from 'url';
+import alibabaRouter from './routes/scraperRoute.js';
+import 'dotenv/config.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT;
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+app.use(express.static(__dirname));
+app.use('/api/scrape', alibabaRouter);
+
+app.get('/', (req, res) => {
+    res.send('Shopify server is running!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Test page available at http://localhost:${PORT}/test.html`);
+});
