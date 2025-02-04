46668cdae7bd384c5c3fb29628be4835abc7babd
Jafor Iqbal <jaforiqbal5593@gmail.com>
2025-02-04 20:34:59 +0600

    amazon scraping complete

--- express/routes/scraperRoute.js
+++ express/routes/scraperRoute.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { scrapeProducts } from "../middlewares/alibabaScraper.js";
+import { scrapeAmazonProducts } from "../middlewares/amazonScraper.js";
 
 const router = Router();
 
 router.post("/alibaba", scrapeProducts);
+router.post("/amazon", scrapeAmazonProducts);
 
 export default router;
