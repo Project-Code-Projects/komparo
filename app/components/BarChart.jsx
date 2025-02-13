cb31c978337d1478386f55fb3d077c7b1c941e38
asif93-138 <asif93@student.sust.edu>
2025-02-14 01:08:44 +0600

    graph plotting for analytics almost done!

--- app/components/BarChart.jsx
+++ app/components/BarChart.jsx
@@ -1,25 +1,38 @@
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const data = [
-  { name: 'Product A', value: 400 },
-  { name: 'Product B', value: 300 },
-  { name: 'Product C', value: 200 },
-  { name: 'Product D', value: 278 },
+  { price: 10, nop: 400 },
+  { price: 20, nop: 300 },
+  { price: 30, nop: 200 },
+  { price: 40, nop: 278 },
 ];
 
 export default function BarChartGraph() {
+  
+  function CustomTooltip({ payload, label, active }) {
+    if (active) {
+      return (
+        <p style={{backgroundColor: 'white', padding: '8px'}}>
+          <span>{`Price: ${label}`}</span><br /><span>{`NOP: ${payload[0].value}`}</span>
+        </p>
+      );
+    }
+  
+    return null;
+  }
   return (
-    <div className="w-full h-80">
-      <ResponsiveContainer width="100%" height={300}>
-        <BarChart data={data} barSize={40}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="value" fill="#8884d8" />
-        </BarChart>
-      </ResponsiveContainer>
-    </div>
+    <>
+        <ResponsiveContainer width="100%" height={300}>
+    <BarChart data={data} barSize={40}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="price" />
+      <YAxis />
+      <Tooltip content={<CustomTooltip />} />
+      <Legend />
+      <Bar dataKey="nop" fill="#8884d8" />
+    </BarChart>
+  </ResponsiveContainer>
+  <p style={{textAlign: 'center'}}>Graph : Representation of price vs number of sold products(NOP).</p>
+    </>
   );
 }
--- app/components/LineChart.jsx
+++ app/components/LineChart.jsx
@@ -1,23 +1,39 @@
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const data = [
-  { name: 'Jan', uv: 4000 },
-  { name: 'Feb', uv: 3000 },
-  { name: 'Mar', uv: 2000 },
-  { name: 'Apr', uv: 2780 },
+  { date: 'Feb 1', price: 4000 },
+  { date: 'Feb 2', price: 3000 },
+  { date: 'Feb 3', price: 2000 },
+  { date: 'Feb 4', price: 2780 },
 ];
 
 export default function LineChartGraph() {
+  
+  function CustomTooltip({ payload, label, active }) {
+    
+    if (active) {
+      return (
+        <p style={{backgroundColor: 'white', padding: '8px'}}>
+          <span>{`Date: ${label}`}</span><br /><span>{`Price: ${payload[0].value}`}</span>
+        </p>
+      );
+    }
+  
+    return null;
+  }
   return (
-    <ResponsiveContainer width='100%' height={300}> 
+    <>
+        <ResponsiveContainer width='100%' height={300}> 
       <LineChart data={data}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
+        <XAxis dataKey="date" />
         <YAxis />
-        <Tooltip />
+        <Tooltip content={<CustomTooltip />} />
         <Legend />
-        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <Line type="monotone" dataKey="price" stroke="#8884d8" />
       </LineChart>
     </ResponsiveContainer>
+    <p style={{textAlign: 'center'}}>Graph : Representation of price fluctuation over time.</p>
+    </>
   );
 }
