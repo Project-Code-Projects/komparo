c1046019b0b910b58b678622aaa8764b409cca2f
asif93-138 <asif93@student.sust.edu>
2025-02-24 13:49:27 +0600

    getScrappedProducts controller updated!

--- express/controllers/productsController.js
+++ express/controllers/productsController.js
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-import { downloadCsv } from '../utils/cloudinaryUtils.js';
+// import { downloadCsv } from '../utils/cloudinaryUtils.js';
 
 const prisma = new PrismaClient();
 
@@ -11,6 +11,8 @@ export const getScrappedProducts = async (req, res) => {
 
         let { query } = req.query;
 
+        // console.log('Query :', query);
+
         if (!query) {
             return res.status(400).json({ error: "Query parameter is required" });
         }
@@ -21,39 +23,38 @@ export const getScrappedProducts = async (req, res) => {
         const comparator = await prisma.comparator.findUnique({
             where: { query }
         });
-
+        // console.log(comparator);
         if (comparator.status === "pending") {
             return res.status(202).json({
                 message: "Data is still being processed. Please check back in a few hours..."
             });
         }
 
-        const { amazon: amazon_url, alibaba: alibaba_url } = comparator;
+        const result = await prisma.scrapeData.findMany({
+            where: { comparatorQuery: query }
+        });
+        // console.log(result);
+        // const { amazon: amazon_url, alibaba: alibaba_url } = comparator;
 
-        console.log("Amazon URL:", amazon_url || "No Amazon URL available");
-        console.log("Alibaba URL:", alibaba_url || "No Alibaba URL available");
+        // console.log("Amazon URL:", amazon_url || "No Amazon URL available");
+        // console.log("Alibaba URL:", alibaba_url || "No Alibaba URL available");
 
-        let amazonProducts;
-        let alibabaProducts;
+        // let amazonProducts;
+        // let alibabaProducts;
 
-        if (alibaba_url) {
-            console.log("Downloading from Alibaba URL:", alibaba_url);
-            // alibabaProducts = (await downloadCsv(alibaba_url)).slice(0, 3);
-            alibabaProducts = (await downloadCsv(alibaba_url));
-        }
-        if (amazon_url) {
-            console.log("Downloading from Amazon URL:", amazon_url);
-            // amazonProducts = (await downloadCsv(amazon_url)).slice(0, 3);
-            amazonProducts = (await downloadCsv(amazon_url));
-        }
+        // if (alibaba_url) {
+        //     console.log("Downloading from Alibaba URL:", alibaba_url);
+        //     // alibabaProducts = (await downloadCsv(alibaba_url)).slice(0, 3);
+        //     alibabaProducts = (await downloadCsv(alibaba_url));
+        // }
+        // if (amazon_url) {
+        //     console.log("Downloading from Amazon URL:", amazon_url);
+        //     // amazonProducts = (await downloadCsv(amazon_url)).slice(0, 3);
+        //     amazonProducts = (await downloadCsv(amazon_url));
+        // }
 
         console.log("[END]")
-        res.status(200).json({
-            alibaba_url,
-            alibaba: alibabaProducts,
-            amazon_url,
-            amazon: amazonProducts,
-        });
+        res.status(200).json(result);
 
     } catch (error) {
         console.error("Error fetching products:", error);
